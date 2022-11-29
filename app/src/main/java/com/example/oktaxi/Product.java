4d6f233ecea8937f227fbdce70567311963795ee
javier <javierzv777@gmail.com>
2022-11-28 20:39:58 -0500

    ok taxi, ciclo 4 sprint 1

---
+++ app/src/main/java/com/example/oktaxi/Product.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Product extends AppCompatActivity {
+    private Button btnProductInfo;
+    private TextView textProductTitle, textProductDes;
+    private ImageView imgProduct;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+        btnProductInfo = (Button) findViewById(R.id.btnProductInfo);
+        textProductTitle = (TextView) findViewById(R.id.textProductTitle);
+        textProductDes = (TextView) findViewById(R.id.textProductDes);
+        imgProduct = (ImageView) findViewById(R.id.imgProduct);
+
+        Intent intentIn = getIntent();
+        textProductTitle.setText(intentIn.getStringExtra("title"));
+        textProductDes.setText(intentIn.getStringExtra("description"));
+        int codeImage = intentIn.getIntExtra("imageCode",0);
+        imgProduct.setImageResource(codeImage);
+
+        btnProductInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
