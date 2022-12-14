73feba0999d9008ca612d78bda87612827eade5f
javier <javierzv777@gmail.com>
2022-12-14 00:02:44 -0500

    estoy cansado pero creo que va quedando bien, falta 18 minutos del video de autenticacion

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         btnInit.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Intent intent = new Intent(getApplicationContext(), Home.class);
+                        Intent intent = new Intent(getApplicationContext(), Login.class);
                         startActivity(intent);
 
                     }
