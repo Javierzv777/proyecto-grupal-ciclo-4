43b3ab602005ac6cbf4c2b20b232693867ec4a35
Javier <javierzv777@gmail.com>
2022-12-17 17:28:51 -0500

    muestra el mapa

---
+++ app/src/main/java/com/example/myapplication/Maps.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+
+public class Maps extends AppCompatActivity {
+    private MapView map;
+    private MapController mapController;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
+        map = (MapView) findViewById(R.id.map);
+        map.setBuiltInZoomControls(true);
+        mapController = (MapController) map.getController();
+        GeoPoint colombia = new GeoPoint(4.570868,-74.297333);
+        mapController.setCenter(colombia);
+        mapController.setZoom(8);
+        map.setMultiTouchControls(true);
+       // Context context = getApplicationContext();
+
+        MapEventsReceiver mapEventsReceiver = new MapEventsReceiver() {
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint p) {
+                Toast.makeText(getApplicationContext(), "Latitud: " + p.getLatitude() +
+                        "Longitud: " + p.getLongitude(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean longPressHelper(GeoPoint p) {
+                return false;
+            }
+        };
+        MapEventsOverlay mapEventsOverlay = new MapEventsOverlay(this, mapEventsReceiver);
+        map.getOverlays().add(mapEventsOverlay);
+    }
+}
