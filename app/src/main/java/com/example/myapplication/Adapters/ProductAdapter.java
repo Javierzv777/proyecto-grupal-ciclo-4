d068b73fbfab9a839170588a2fc44bc28685e808
Javier <javierzv777@gmail.com>
2022-12-21 01:18:05 -0500

    coding asincronismo

--- app/src/main/java/com/example/myapplication/Adapters/ProductAdapter.java
+++ app/src/main/java/com/example/myapplication/Adapters/ProductAdapter.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,8 +15,13 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import com.example.myapplication.DB.DBFirebase;
 import com.example.myapplication.DB.DBHelper;
@@ -34,7 +42,8 @@ public class ProductAdapter extends BaseAdapter implements ComeBackHome {
     private DBFirebase dbFirebase;
     private DBHelper dbHelper;
     ComeBackHome comeBackHome = this;
-    private ProgressDialog progressDialog;
+    public ProgressDialog progressDialog;
+    private ProgressBar progressBar;
 
 
     public ProductAdapter(Context context,ArrayList<Producto> arrayProducts ) {
@@ -63,8 +72,10 @@ public class ProductAdapter extends BaseAdapter implements ComeBackHome {
         View v = view;
         v = layoutInflater.inflate(R.layout.templete_product, null);
 
+
         Producto producto = arrayProducts.get(i);
 
+        progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
         Button btnProductTemplate = (Button) v.findViewById(R.id.btnProductTemplate);
         Button btnEdit = (Button) v.findViewById(R.id.btnEdit);
         Button btnDelete =  (Button) v.findViewById(R.id.btnDelete);
@@ -73,29 +84,56 @@ public class ProductAdapter extends BaseAdapter implements ComeBackHome {
         TextView textViewNam = (TextView) v.findViewById(R.id.textProductTitle);
         TextView textViewDes = (TextView) v.findViewById(R.id.textProductDes);
 
-        String imagen = producto.getImage();
 
+        String imagen = producto.getImage();
+        //progressBar.setVisibility(View.VISIBLE);
 
 
             if(!imagen.equals("")) {
+
                 try{
-                progressDialog = new ProgressDialog(viewGroup.getContext());
-                progressDialog.setTitle("Subiendo...");
-                progressDialog.setMessage("Subiendo imagen a firebase");
-                progressDialog.setCancelable(false);
-                progressDialog.show();
+
                 Picasso.with(viewGroup.getContext())
                         .load(imagen)
                         .resize(300, 300)
-                        .into(imgProduct);
-                progressDialog.dismiss();
+                        .into(imgProduct,  new com.squareup.picasso.Callback() {
+                            @Override
+                            public void onSuccess() {
+                                textViewNam.setText(producto.getName());
+                                textViewDes.setText(producto.getDescription());
+                                progressBar.setVisibility(View.GONE);
+
+
+                            }
+
+                            @Override
+                            public void onError() {
+                                textViewNam.setText(producto.getName());
+                                textViewDes.setText(producto.getDescription());
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        });
+
+
+
+
+
                 }catch(Exception e) {
                     Log.e("Fatal error", e.toString());
+                    progressBar.setVisibility(View.GONE);
                 }
+
+                try {
+                    Thread.sleep(200 );
+                    progressBar.setVisibility(View.GONE);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
             }
 
-        textViewNam.setText(producto.getName());
-        textViewDes.setText(producto.getDescription());
+
 
         btnProductTemplate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -151,6 +189,8 @@ public class ProductAdapter extends BaseAdapter implements ComeBackHome {
         return v;
 
     }
+
+
     @Override
     public void intentToHome() {
         Intent intent = new Intent(context.getApplicationContext(), Home.class);
